da7c77eec99af9a350439a9c0c1a667a25667e73
Marcelo Antonio Silva <marcelojhs@yahoo.com.br>
2021-09-08 18:27:59 -0300

    statics charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,36 @@
+import BarChart from "components/BarChart";
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!</h1> 
-    </div>
-  );
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">DashBoard de Vendas!</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className = "text-center text-secondary"> Taxa de Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+          <h5 className = "text-center text-secondary"> Taxa de Sucesso (%)</h5>
+            <DonutChart />
+          </div>
+          </div>
+          <div className = "py-3">
+          <h2 className="text-primary">Todas Vendas</h2>
+          </div>
+
+          <DataTable />
+        </div>
+        <Footer />
+      </>
+      );
 }
 
-export default App;
+      export default App;
