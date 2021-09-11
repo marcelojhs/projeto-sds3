d15af40edf91a9da27f0438e4308e65834239c0e
Marcelo Antonio Silva <marcelojhs@yahoo.com.br>
2021-09-10 22:27:52 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -4,10 +4,9 @@ import java.io.Serializable;
 
 import com.devsuperior.dsvendas.entities.Seller;
 
-public class SellerDTO  implements Serializable{
-	
-	
+public class SellerDTO  implements Serializable{	
 	private static final long serialVersionUID = 1L;
+	
 	private Long id;
     private String name;
     
@@ -15,13 +14,12 @@ public class SellerDTO  implements Serializable{
     	
     }
 
-	public SellerDTO(Long id, String name) {
-		
+	public SellerDTO(Long id, String name) {	
 		this.id = id;
 		this.name = name;
 	}
 	
-public SellerDTO(Seller entity) {
+     public SellerDTO(Seller entity) {
 		
 		id = entity.getId();
 		name = entity.getName();
--- backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
@@ -10,14 +10,15 @@ import com.devsuperior.dsvendas.dto.SellerDTO;
 import com.devsuperior.dsvendas.entities.Seller;
 import com.devsuperior.dsvendas.repositeries.SellerRepository;
 
+
 @Service
-public class SellerService {
+public class SellerService {  
  
 	@Autowired
 	private SellerRepository repository;
 	
 	public List<SellerDTO> findAll(){
-		List <Seller> result= repository.findAll();
+		List<Seller> result = repository.findAll();
 		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
 	}
 }
